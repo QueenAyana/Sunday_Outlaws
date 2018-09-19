249cca81cc723171070446c28a5d8006c1357e71
Alex Ichikawa <aichikawa@isolitesystems.com>
2018-09-18 21:24:56 -0700

    some working api

--- assets/java/app.js
+++ assets/java/app.js
@@ -1,7 +1,8 @@
 
 let breed;
-
-
+let zip;
+let count = 5;
+let offset = 5;
 
 $("#submit").on("click", function() {
     $.ajax({
@@ -13,3 +14,24 @@ $("#submit").on("click", function() {
      });
 
 });
+
+$("#submit2").on("click", function() {
+    $.ajax({
+        url: 'https://dog.ceo/api/breed/hound/images/random',
+    }).then(function(response) {
+        console.log(response);
+    })
+})
+
+$("#submit3").on("click", function() {
+    $.ajax({
+        url: 'https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=akita+dog&origin=*',
+    }).then(function(response) {
+        console.log(response);
+    })
+})
+
+
+
+
+
